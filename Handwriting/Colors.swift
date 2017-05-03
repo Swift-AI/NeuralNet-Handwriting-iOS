4ab475cbeab0cc6a44a4dd33738c60cb9707d9ae
Collin <collinhundley@gmail.com>
2017-05-02 19:28:24 -0600

    Add app icon and launch image

--- Handwriting/Colors.swift
+++ Handwriting/Colors.swift
@@ -19,10 +19,6 @@ extension UIColor {
         return UIColor(red: 229 / 255, green: 127 / 255, blue: 71 / 255, alpha: alpha)
     }
     
-    static func saiRed(alpha: CGFloat = 1) -> UIColor {
-        return UIColor(red: 221 / 255, green: 72 / 255, blue: 57 / 255, alpha: alpha)
-    }
-    
     static func saiGreen(alpha: CGFloat = 1) -> UIColor {
         return UIColor(red: 61 / 255, green: 255 / 255, blue: 43 / 255, alpha: alpha)
     }
